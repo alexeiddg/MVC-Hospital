b6a9baf5df8d7f757e1488e39cd30499901a9da1
alexeiddg <117191717+alexeiddg@users.noreply.github.com>
2025-03-06 20:08:18 -0600

    + Cita obj class

---
+++ src/main/java/com/alexeiddg/mvcproject/model/object/Cita.java
@@ -0,0 +1,70 @@
+package com.alexeiddg.mvcproject.model.object;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Cita {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fecha;
+    private String hora;
+    private String motivoConsulta;
+    private String estado;
+
+    public Cita() {}
+
+    public Cita(Long id, String fecha, String hora, String motivoConsulta, String estado) {
+        this.id = id;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.motivoConsulta = motivoConsulta;
+        this.estado = estado;
+    }
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public String getMotivoConsulta() {
+        return motivoConsulta;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    // Setters
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public void setMotivoConsulta(String motivoConsulta) {
+        this.motivoConsulta = motivoConsulta;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+}
